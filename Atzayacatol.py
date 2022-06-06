c091792dcfea0d48dacc82685138c802eddfbd37
norindes <norindes@gmail.com>
2022-06-06 00:14:21 -0500

    Database added

--- Atzayacatol.py
+++ Atzayacatol.py
@@ -3,20 +3,24 @@ import requests
 import pandas as pd
 from openpyxl import load_workbook
 import webbrowser
+import mysql.connector
+from mysql.connector import Error
 
 print("Atzayacatol v1 \n ( ͡° ͜ʖ ͡°) \n ")
 
 def menu():
     print("1. Web Scrap HP")
     print("2. Musiquita")
-    print("3. BTC Price")
-    print("4. Salir")
+    print("3. Crear Database")
+    print("4. Crear Tabla")
+    print("5. Agregar Valores")
+    print("6. Salir")
     
 menu()
 
 option = int(input("Selecciona tu destino \n"))
 
-while option != 4:
+while option != 6:
     if option ==1:
 #variables para las urls
         CC = input("Saca CC:\n")
@@ -46,7 +50,7 @@ while option != 4:
             specsDF.to_csv("export.csv", index=False)
             accDF.to_csv("export2.csv", index=False)
     
-    #limpia DF1
+#limpia DF1
             a_file = open("export.csv", "r", encoding='utf8')
             lines = a_file.readlines()
             a_file.close()
@@ -56,7 +60,7 @@ while option != 4:
                     new_file.write(line)
             new_file.close()
     
-    #limpia DF2
+#limpia DF2
             a_file2 = open("export2.csv", "r", encoding='utf8')
             lines2 = a_file2.readlines()
             a_file2.close()
@@ -66,11 +70,11 @@ while option != 4:
                 new_file2.write(line2)
             new_file2.close()
     
-    #pasa a xlsx
+#pasa a xlsx
             read_file = pd.read_csv (r'export.csv')
             read_file.to_excel (r'chido.xlsx', index = None, header=True)
     
-    #agrega el sheet de accesorios
+#agrega el sheet de accesorios
             book = load_workbook("chido.xlsx")
             writer = pd.ExcelWriter("chido.xlsx", engine = 'openpyxl')
             writer.book = book
@@ -87,14 +91,89 @@ while option != 4:
         break
     
     elif option ==3:
-        
+#conecta a SQL
+        def create_connection(host_name, user_name, user_password):
+            connection = None
+            try:
+                connection = mysql.connector.connect(
+                    host=host_name,
+                    user=user_name,
+                    passwd=user_password,
+                    
+                )
+                print("Conectado a SQL")
+            except Error as e:
+                print(f"Error '{e}'")
+            return connection
 
+        connection = create_connection("localhost", "root", "1234")
+        
+#creame la DB
+        def create_database(connection, query):
+            cursor = connection.cursor()
+            try:
+                cursor.execute(query)
+
+                print("DB creada")
+            except Error as e:
+                print(f"Error '{e}'")
+                
+        dbname = input("Saca el nombre de la DB:\n")
+        create_database_query = "CREATE DATABASE "+dbname
+        print(create_database_query)
+        create_database(connection, create_database_query) 
+                        
         break
     
-     
+    elif option ==4:
+        db_name = input("A que DB le agregas la tabla? :\n")
+        def create_connection(host_name, user_name, user_password, db_name):
+            connection = None
+            try:
+                connection = mysql.connector.connect(
+                    host=host_name,
+                    user=user_name,
+                    passwd=user_password,
+                    database=db_name
+                )
+                print("Conectado a SQL")
+            except Error as e:
+                print(f"Error '{e}'")
+            return connection
+
+        connection = create_connection("localhost", "root", "1234", db_name)
+                
+        def execute_query(connection, query):
+            cursor = connection.cursor()
+            try:
+                cursor.execute(query)
+                connection.commit()
+                print("Tabla insertada")
+            except Error as e:
+                print(f"Error '{e}'")
+                
+        create_users_table = """
+        CREATE TABLE IF NOT EXISTS Users (
+            id INT AUTO_INCREMENT, 
+            name TEXT NOT NULL, 
+            age INT, 
+            gender TEXT, 
+            nationality TEXT, 
+            PRIMARY KEY (id)
+            ) ENGINE = InnoDB
+        """
+
+        execute_query(connection, create_users_table)
+        break
+    elif option ==5:
+        print("wip")
+                
+        break
+         
     else:
         print("Error: opcion invalida")
+        
         break
-print("Toma agua, bye")
+print("Terminated")
 
         
